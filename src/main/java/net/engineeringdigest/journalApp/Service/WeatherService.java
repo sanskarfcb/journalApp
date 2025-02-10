c818d3d1d8c30cabaf281986598616090b0322c0
sanskarfcb <sanskarshashwat.vns@gmail.com>
2025-02-10 16:45:29 +0530

    xzy

--- src/main/java/net/engineeringdigest/journalApp/Service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/Service/WeatherService.java
@@ -2,17 +2,21 @@ package net.engineeringdigest.journalApp.Service;
 
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpEntity;
 
-@Component
+@Service
 public class WeatherService {
 
-    private static final String apikey = "8adcff20b069ec0dc6ec385dfe09f343";
+
+    @Value("${weather.api.key}")
+    private String apikey ;
     private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
